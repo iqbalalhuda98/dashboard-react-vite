c34c96ecd6aee006a6befb092ae91a5466680af1
Iqbal Alhuda <iqbalalhuda1998@gmail.com>
2024-06-18 01:42:14 +0700

    feat darkmode toggle

--- src/components/AppLayout/AppHeader/AppHeader.jsx
+++ src/components/AppLayout/AppHeader/AppHeader.jsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "@/components/ThemeProvider/ModeToggle/ModeToggle"
+
 const AppHeader = () => {
     return (
         <div className="w-full h-full flex items-center justify-end px-4 font-bold gap-4">
             YOUR DASHBOARD
+            <ModeToggle />
         </div>
     )
 }
